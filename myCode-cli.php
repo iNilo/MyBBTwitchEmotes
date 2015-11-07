118fce73e903de4fba485a2671d9a2ba9d511e17
iNilo <iNilo@users.noreply.github.com>
2015-11-07 14:41:36 +0100

    Config parameters

--- myCode-cli.php
+++ myCode-cli.php
@@ -1,4 +1,10 @@
 <?php
+//configuration here:
+//the table name of mybb
+$mybb_table_name = "mybb";
+//what you want to use for subscriber trigger (default is colon)
+$subscriber_activator = ":";
+
 Echo "First off, lets delete all the old files.\n";
 $twitch_full_file = __DIR__ . DIRECTORY_SEPARATOR .'ExportedSQL' . DIRECTORY_SEPARATOR . 'twitch_all_emotes.sql';
 $twitch_general_files = __DIR__ . DIRECTORY_SEPARATOR .'ExportedSQL' . DIRECTORY_SEPARATOR . 'twitch_general_emotes.sql';
@@ -27,7 +33,7 @@ foreach ($global_data->emotes as $name => $emote_detail)
     $sql_regex = '('.$name.')\\\b';
     $sql_replacement = '<img src="https://static-cdn.jtvnw.net/emoticons/v1/'.$emote_id.'/1.0" alt="'.$name.'"/>';
     $sql_active = true;
-    $lines .= "INSERT INTO `mybb`.`mybb_mycode` (`cid`, `title`, `description`, `regex`, `replacement`, `active`, `parseorder`) VALUES (NULL, '".$sql_title."', '".mysqli_real_escape_string($link,$sql_description)."', '".$sql_regex."', '".$sql_replacement."', '1', '0');\n";
+    $lines .= "INSERT INTO `".$mybb_table_name."`.`mybb_mycode` (`cid`, `title`, `description`, `regex`, `replacement`, `active`, `parseorder`) VALUES (NULL, '".$sql_title."', '".mysqli_real_escape_string($link,$sql_description)."', '".$sql_regex."', '".$sql_replacement."', '1', '0');\n";
     $general_count++;
 }   
 //poop it out into a file
@@ -51,10 +57,10 @@ foreach ($channel_data->channels as $channel)
     {
         $sql_title = $emote->code;
         $sql_description = $emote->code;
-        $sql_regex = ':'.$emote->code.':';
+        $sql_regex = $subscriber_activator.$emote->code.$subscriber_activator;
         $sql_replacement = '<img src="https://static-cdn.jtvnw.net/emoticons/v1/'.$emote->image_id.'/1.0" alt="'.$emote->code.'"/>';
         $sql_active = true;
-        $lines .= "INSERT INTO `mybb`.`mybb_mycode` (`cid`, `title`, `description`, `regex`, `replacement`, `active`, `parseorder`) VALUES (NULL, '".$sql_title."', '".mysqli_real_escape_string($link,$sql_description)."', '".$sql_regex."', '".$sql_replacement."', '1', '0');\n";
+        $lines .= "INSERT INTO `".$mybb_table_name."`.`mybb_mycode` (`cid`, `title`, `description`, `regex`, `replacement`, `active`, `parseorder`) VALUES (NULL, '".$sql_title."', '".mysqli_real_escape_string($link,$sql_description)."', '".$sql_regex."', '".$sql_replacement."', '1', '0');\n";
         $subscriber_count++;
     }
 
